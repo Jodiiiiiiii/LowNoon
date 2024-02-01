22fee6286779fdd4a99bdc99f2e6f02aefea78af
Andrew Kohler <awk480@gmail.com>
2024-01-31 23:00:29 -0500

    Adding a controller script for Upgrade object

--- Assets/Scripts/Decoratives/Tumbleweed/TumbleweedSpawner.cs
+++ Assets/Scripts/Decoratives/Tumbleweed/TumbleweedSpawner.cs
@@ -7,18 +7,18 @@ using UnityEngine;
 public class TumbleweedSpawner : MonoBehaviour
 {
     
-    [SerializeField] private float length = 25f;    // The length of the area that tumbleweeds can spawn from
-    [SerializeField] private float spawnInterval;   // How often a new tumbleweed is spawned
-    [SerializeField] private GameObject tumbleweedPrefab;   // The tumbleweed to spawn
+    [SerializeField] private float _length = 25f;    // The length of the area that tumbleweeds can spawn from
+    [SerializeField] private float _spawnInterval;   // How often a new tumbleweed is spawned
+    [SerializeField] private GameObject _tumbleweedPrefab;   // The tumbleweed to spawn
 
     private float min;  // The leftmost extent of the spawn line
     private float max;  // The rightmost extent of the spawn line
     void Start()
     {
         // Setting all of our variables using the inputs from the Inspector window
-        min = this.transform.position.x - length;
-        max = this.transform.position.x + length;
-        InvokeRepeating("SpawnTumbleweed", 0, spawnInterval);
+        min = this.transform.position.x - _length;
+        max = this.transform.position.x + _length;
+        InvokeRepeating("SpawnTumbleweed", 0, _spawnInterval);
     }
 
     // Update is called once per frame
@@ -29,6 +29,6 @@ public class TumbleweedSpawner : MonoBehaviour
 
     void SpawnTumbleweed()
     {
-        Instantiate(tumbleweedPrefab, new Vector3(Random.Range(min, max + 1), this.transform.position.y, this.transform.position.z), new Quaternion());
+        Instantiate(_tumbleweedPrefab, new Vector3(Random.Range(min, max + 1), this.transform.position.y, this.transform.position.z), new Quaternion());
     }
 }
---
+++ Assets/Scripts/UpgradeController.cs
@@ -0,0 +1,75 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UpgradeController : MonoBehaviour
+{
+    // TODO
+    // Look at player
+    // A method that applies the effect of the upgrade to the player
+        // OnTriggerEnter? Are we doing a player or item centric approach?
+    // Devise a way to spawn 3 of these and destroy them all when one is chosen
+
+    public enum UpgradeType
+    {
+        Health,
+        Armor,
+        Damage,
+        FireSpeed,
+        MoveSpeed,
+        Light
+    };
+
+    [SerializeField] private List<GameObject> modelOptions;
+
+    public bool RandomizeOnStartup = true;  // When this upgrade spawns, its type is randomly selected if this is true
+    public UpgradeType Type; // What upgrade is this?
+
+    void Start()
+    {
+        if (RandomizeOnStartup)
+        {
+            RandomlyChooseType();
+        }
+        setModel(Type);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void RandomlyChooseType()
+    {
+        int random = Random.Range(0, 6);
+        if(random == 0)
+            Type = UpgradeType.Health;
+        else if(random == 1)
+            Type = UpgradeType.Armor;
+        else if(random == 2)
+            Type = UpgradeType.Damage;
+        else if(random == 3)
+            Type = UpgradeType.FireSpeed;
+        else if(random == 4)
+            Type = UpgradeType.MoveSpeed;
+        else if(random == 5)
+            Type = UpgradeType.Light;
+    }
+
+    private void setModel(UpgradeType type) // Set the model for whatever we want this upgrade to be
+    {
+        if (type == UpgradeType.Health)
+            modelOptions[0].SetActive(true);
+        else if (type == UpgradeType.Armor)
+            modelOptions[1].SetActive(true);
+        else if (type == UpgradeType.Damage)
+            modelOptions[2].SetActive(true);
+        else if (type == UpgradeType.FireSpeed)
+            modelOptions[3].SetActive(true);
+        else if (type == UpgradeType.MoveSpeed)
+            modelOptions[4].SetActive(true);
+        else if(type == UpgradeType.Light)
+            modelOptions[5].SetActive(true);
+    }
+}
