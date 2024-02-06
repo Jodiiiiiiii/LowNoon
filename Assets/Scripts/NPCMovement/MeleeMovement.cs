6cc1c82f436cb71f72aa94d2371664a1ca5e047f
Jacob <jacob.pyatt102@gmail.com>
2024-02-06 16:50:17 -0500

    Enemies Now experience gravity
    Had to redo how enemy movement is calculated.

--- Assets/Scripts/NPCMovement/MeleeMovement.cs
+++ Assets/Scripts/NPCMovement/MeleeMovement.cs
@@ -1,38 +1,45 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class MeleeMovement : MonoBehaviour
 {
-    public GameObject Player;
-    public Vector3 PlayerPosition;
-
-    public GameObject Ground;
-    public Vector3 GroundPosition;
+    private GameObject _Player;
+    private Vector3 _PlayerPosition;
 
     public float MoveSpeed = 5f;
+
+    private Rigidbody _rigidBody;
 
 
     // Start is called before the first frame update
     void Start()
     {
-        Player = GameObject.FindWithTag("Player");
-        Ground = GameObject.FindWithTag("Ground");
+        _Player = GameObject.FindWithTag("Player");
+        _rigidBody = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
+    // Reference: https://www.youtube.com/watch?v=ZExSz7x69j8
     void Update()
     {
-        PlayerPosition = Player.transform.position;
+        _PlayerPosition = _Player.transform.position;
 
         // Set enemy rotation to face player
-        transform.LookAt(PlayerPosition);
+        transform.LookAt(_PlayerPosition);
 
         // Move enemy towards player
         // If enemy is within 1 unit of player, stop moving
-        if (Vector3.Distance(PlayerPosition, transform.position) > 1)
+        if (Vector3.Distance(_PlayerPosition, transform.position) > 2f)
         {
-            transform.position += transform.forward * MoveSpeed * Time.deltaTime;
+            Vector3 direction = (_PlayerPosition - transform.position).normalized * MoveSpeed;
+            direction.y = _rigidBody.velocity.y;
+            _rigidBody.velocity = direction;
+        }
+        else
+        {
+            _rigidBody.velocity = new Vector3(0, 0, 0);
         }
     }
 }
