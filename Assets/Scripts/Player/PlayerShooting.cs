e97f9429988eb1990f9a9ea5dbb9b6a7e49b82fe
Jodi Foster <33043348+JohhnSmithh@users.noreply.github.com>
2024-02-15 09:44:11 -0500

    PlayerShooting formatting
    whitespace, naming conventions, comments

--- Assets/Scripts/Player/PlayerShooting.cs
+++ Assets/Scripts/Player/PlayerShooting.cs
@@ -2,42 +2,40 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+/// <summary>
+/// handles inputs for player shooting, determines if it is a valid time to shoot, and creates bullets
+/// </summary>
 public class PlayerShooting : MonoBehaviour
 {
-    // Stores prefab of bullet object
+
     [Tooltip("stores prefab of bullet object")] public GameObject BulletObject;
 
     [Tooltip("how long before player can shoot again (seconds)")] public float CooldownTime = 0.0f;
 
-    //Reference to player object script
+    float _timer = 0.0f;
     PlayerController _playerController;
-
-    
-
-    float timer = 0.0f;
-    
-    
     
     // Start is called before the first frame update
     void Start()
     {
-        
+        _playerController = GameObject.Find("Player").GetComponent<PlayerController>();
     }
 
     // Update is called once per frame
     void Update()
-    { 
-        if (Input.GetKeyDown(KeyCode.Mouse0) && timer > CooldownTime) //&& playerController.State == CharacterState.Stationary) // shooting command AND not during cooldown time !!! AND in stationary state
+    {
+        // shooting input AND not during cooldown time AND in stationary state
+        if (Input.GetKeyDown(KeyCode.Mouse0) && _timer > CooldownTime && _playerController.State == CharacterState.Stationary)
         {
             GameObject newBullet = Instantiate(BulletObject);
 
-            newBullet.transform.position = transform.position + (0.5f * transform.forward); // somehow coming out of the gun 
+            // TODO: add configurable parameters so that bullet spawns coming out of gun
+            newBullet.transform.position = transform.position + (0.5f * transform.forward); // temporary
             newBullet.GetComponent<Rigidbody>().AddForce(newBullet.GetComponent<BulletStats>().InitialForce * transform.forward, ForceMode.Impulse);
 
-            timer = 0.0f;
-
+            _timer = 0.0f;
         }
 
-        timer += Time.deltaTime;
+        _timer += Time.deltaTime;
     }
 }
