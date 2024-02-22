13967aa1e0ded3ed704afe8511fa24131a0f6204
Dayanita <dayanitakumar@ufl.edu>
2024-02-21 23:44:46 -0500

    Added armor feature to damage receiving

--- Assets/Scripts/Player/PlayerHealth.cs
+++ Assets/Scripts/Player/PlayerHealth.cs
@@ -28,20 +28,27 @@ public class NewBehaviourScript : MonoBehaviour
 
             // destroy bullet always on impact with damage receiver
             Destroy(DamagerObject);
-            Debug.Log(GameManager.Instance.PlayerData.CurrHealth);
+            
 
+
+            GameManager.Instance.PlayerData.Armor -= bulletStats.DamageLevel;
+            if (GameManager.Instance.PlayerData.Armor < 0)
+            {
+                GameManager.Instance.PlayerData.CurrHealth += GameManager.Instance.PlayerData.Armor;
+                GameManager.Instance.PlayerData.Armor = 0;
+            }
+            
             // check if player reaches 0 health
-            GameManager.Instance.PlayerData.CurrHealth -= bulletStats.DamageLevel;
-
             if (GameManager.Instance.PlayerData.CurrHealth <= 0)
             {
-                Debug.Log("dead"); // player dies
+                // player dies?
             }
         }
 
 
 
         //if armor upgrade?? then currHealth += armor. then same effect happens?
+        // += on the stats script lol?
 
     }
 }
