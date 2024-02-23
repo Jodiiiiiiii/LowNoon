9e82bb6c34d97c39cc1f898df515f2d66c0e8897
Dayanita <dayanitakumar@ufl.edu>
2024-02-23 12:32:48 -0500

    adding invulnerable state

--- Assets/Scripts/Player/PlayerHealth.cs
+++ Assets/Scripts/Player/PlayerHealth.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class NewBehaviourScript : MonoBehaviour
 {
+    public bool InvulnerableState = false;
+    public float InvulnerableTime = 5.0f;
+    public float timer = 0.0f;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,7 +16,11 @@ public class NewBehaviourScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        timer += Time.deltaTime;
+        if (timer > InvulnerableTime)
+        {
+            InvulnerableState = false;
+        }
     }
 
     private void OnCollisionEnter(Collision collision)
@@ -26,29 +33,30 @@ public class NewBehaviourScript : MonoBehaviour
             BulletStats bulletStats = DamagerObject.GetComponent<BulletStats>();
             
 
-            // destroy bullet always on impact with damage receiver
+            // destroy bullet always on impact with player
             Destroy(DamagerObject);
-            
 
 
-            GameManager.Instance.PlayerData.Armor -= bulletStats.DamageLevel;
-            if (GameManager.Instance.PlayerData.Armor < 0)
+            if (!InvulnerableState)
             {
-                GameManager.Instance.PlayerData.CurrHealth += GameManager.Instance.PlayerData.Armor;
-                GameManager.Instance.PlayerData.Armor = 0;
+                GameManager.Instance.PlayerData.Armor -= bulletStats.DamageLevel;
+                if (GameManager.Instance.PlayerData.Armor < 0)
+                {
+                    GameManager.Instance.PlayerData.CurrHealth += GameManager.Instance.PlayerData.Armor;
+                    GameManager.Instance.PlayerData.Armor = 0;
+                }
+                InvulnerableState = true;
+                timer = 0.0f; // resets timer
             }
             
             // check if player reaches 0 health
             if (GameManager.Instance.PlayerData.CurrHealth <= 0)
             {
-                // player dies?
+                // player dies
             }
         }
 
-
-
-        //if armor upgrade?? then currHealth += armor. then same effect happens?
-        // += on the stats script lol?
+        
 
     }
 }
