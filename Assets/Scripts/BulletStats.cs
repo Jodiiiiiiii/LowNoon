092612c1780453dd94ef0b675154a0a86c07d259
Jodi Foster <33043348+JohhnSmithh@users.noreply.github.com>
2024-02-28 17:05:21 -0500

    BulletStats tooltip

--- Assets/Scripts/BulletStats.cs
+++ Assets/Scripts/BulletStats.cs
@@ -11,9 +11,9 @@ public class BulletStats : MonoBehaviour
     [Tooltip("Amount of damage dealt on impact to damage reciever (must be an integer for enemy bullets)")] public float DamageLevel = 1;
     [Tooltip("Impulse force magnitude used to launch projectile")] public float InitialForce = 50.0f;
     [Tooltip ("How long before bullet is destroyed (seconds)")] public float BulletLifetime = 3.0f;
-    
+    [Tooltip("Whether or not the 'bullet' / damager is destroyed when it hits something")] public bool IsBullet;
+
     float _timer = 0.0f;
-    public bool IsBullet;
 
     // Start is called before the first frame update
     void Start()
