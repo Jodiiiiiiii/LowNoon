d0f8b66c7857b4612b1c01c33eafe9b45633868f
Andrew Kohler <awk480@gmail.com>
2024-02-29 09:49:58 -0500

    Tied move speed animation to a move speed based multiplier

--- Assets/Scripts/Player/PlayerAnimator.cs
+++ Assets/Scripts/Player/PlayerAnimator.cs
@@ -9,13 +9,12 @@ public class PlayerAnimator : MonoBehaviour
     [SerializeField] private Animator _animator;
 
     // Accessory animation essentials
-    [SerializeField, Tooltip("The model for the player's gun")] private GameObject _gun;
     [SerializeField, Tooltip("The model for the player's hat")] private GameObject _hat;
     [SerializeField, Tooltip("The model for the player's lamp")] private GameObject _lamp;
-    //[SerializeField] private Animator _gunAnimator;  // The gun animator is used for having the gun be either idle or firing
     private Animator _hatAnimator;  // The hat and lamp animators are used p. much exclusively for the death animation
     private Animator _lampAnimator;
 
+    // Gun GameObject and animator no longer required now that gun is parented to player + doesn't need the firing animation
 
 
     private void OnEnable()
@@ -31,29 +30,25 @@ public class PlayerAnimator : MonoBehaviour
     void Start()
     {
         _playerController = GetComponent<PlayerController>();
-
-        //_gunAnimator = _gun.GetComponent<Animator>();
-        //_gun.SetActive(false);
     }
 
     // Update is called once per frame
     void Update()
     {
+        _animator.SetFloat("moveSpdMult", GameManager.Instance.PlayerData.MoveSpeed); // Move speed multiplier so animation stays tuned to actual speed
+
+        // Player animator is primarily controlled by whether the player is (a) dashing and (b) moving
         if(_playerController.State == CharacterState.STATIONARY)
         {
             if (_playerController.State == CharacterState.DASH)
             {
                 StopAllCoroutines();
-                _animator.SetBool("isDashing", true);
-                //_gun.SetActive(false);
-                
-                
+                _animator.SetBool("isDashing", true);             
             }
             else
             {
                 _animator.SetBool("isDashing", false);
                 _animator.SetBool("isMoving", false);
-                //_gun.SetActive(true);
             }
             
         }
@@ -63,8 +58,6 @@ public class PlayerAnimator : MonoBehaviour
             {
                 StopAllCoroutines();
                 _animator.SetBool("isDashing", true);
-               // _gunAnimator.SetBool("fire", false);
-                //_gun.SetActive(false);
                 
             }
             else
@@ -72,32 +65,24 @@ public class PlayerAnimator : MonoBehaviour
                 StopAllCoroutines();
                 _animator.SetBool("isDashing", false);
                 _animator.SetBool("isMoving", true);
-                //_gun.SetActive(false);
             }
             
         }
-
-        //Debug.Log(_animator.GetBool("isDashing"));
-
    
     }
 
-    private void fireGun()
+    private void fireGun() // Method that does prep for the coroutine and calls it (can't call a coroutine with a delegate)
     {
         StopAllCoroutines();
         _animator.Play("Idle", 0, 0);
         StartCoroutine(DoFireGun());
     }
 
-    private IEnumerator DoFireGun()
+    private IEnumerator DoFireGun() // Unique sequence for firing the gun
     {
         _animator.Play("Fire", 0, 0);
-       // _gunAnimator.Play("Gunfire", 0, 0);
         yield return new WaitForSeconds(.5f);
-        
-        //yield return new WaitForSeconds(.5f);
         _animator.Play("Idle", 0, 0);
-        //_gunAnimator.Play("Idle", 0, 0);
 
     }
 }
