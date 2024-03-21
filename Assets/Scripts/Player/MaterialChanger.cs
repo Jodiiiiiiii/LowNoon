15baf220d5a6dc800dd2ac90d1cd815cbc0b3898
Andrew Kohler <awk480@gmail.com>
2024-03-21 13:12:31 -0400

    Bug fix so that material is affected instead of sharedMaterial

--- Assets/Scripts/Player/MaterialChanger.cs
+++ Assets/Scripts/Player/MaterialChanger.cs
@@ -5,17 +5,26 @@ using UnityEngine;
 public class MaterialChanger : MonoBehaviour
 {
     // Start is called before the first frame update
-    [SerializeField] private Material _wormMaterial;
+    private Material _wormMaterial;
     [SerializeField] private Texture _normalTexture;
     [SerializeField] private Texture _gummyTexture;
 
-    private bool _materialChanged = false;  
+    private bool _materialChanged = false;
+
+    private void Start()
+    {
+        _wormMaterial = GameObject.Find("4_Worm").GetComponent<SkinnedMeshRenderer>().material;
+        if(_wormMaterial.mainTexture.name == "WormGummy")
+        {
+            _materialChanged = true;
+        }
+    }
 
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Player"))
         {
-            if(_materialChanged)
+            if (_materialChanged)
                 _wormMaterial.mainTexture = _normalTexture;
             else
                 _wormMaterial.mainTexture = _gummyTexture;
