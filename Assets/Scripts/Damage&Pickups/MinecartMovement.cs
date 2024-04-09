6d4c894f315b3aa5c63641b535eb6131d6860b68
Jodi Foster <33043348+JohhnSmithh@users.noreply.github.com>
2024-04-09 13:22:36 -0400

    Minecart fixes
    polish and variable tuning

--- Assets/Scripts/Damage&Pickups/MinecartMovement.cs
+++ Assets/Scripts/Damage&Pickups/MinecartMovement.cs
@@ -7,7 +7,7 @@ public class MinecartMovement : MonoBehaviour
     // Start is called before the first frame update
     [SerializeField] private float _speed = 50.0f;
     [SerializeField] private GameObject _explosionParticles;
-    [SerializeField] private float _lifetime = 5.0f;
+    [SerializeField] private float _lifetime = 15.0f;
 
     public float ExplosionRadius;
     public int DamageAmount;
@@ -18,34 +18,26 @@ public class MinecartMovement : MonoBehaviour
         _rb = gameObject.GetComponent<Rigidbody>();
         _rb.velocity = transform.forward * _speed;
         Invoke("DestroyMe", _lifetime);
-        
     }
 
     // Update is called once per frame
     void Update()
     {
-        
-        
     }
 
     void OnCollisionEnter(Collision collision){
         if(collision.collider.tag == "Player" || collision.collider.tag == "Enemy"){
             Instantiate(_explosionParticles, gameObject.transform.position, gameObject.transform.rotation);
             Collider[] damagedObjects = Physics.OverlapSphere(transform.position, ExplosionRadius);
-                foreach (var obj in damagedObjects)
-                {
-                    if(obj.tag == "Player"){
-                        obj.GetComponent<PlayerHealth>().ExplosionDmg = DamageAmount;
-                    }
-                    else if(obj.GetComponent<DamageReceiver>() != null){
-                        //Debug.Log("enemy");
-                        // question mark question mark question mark
-                        DamageReceiver healthLevel = obj.GetComponent<DamageReceiver>();
-                        healthLevel.HealthLevel -= (float)DamageAmount;
-                        //Debug.Log("Other health: " + healthLevel.HealthLevel);
-
-                    }
+            foreach (var obj in damagedObjects)
+            {
+                if(obj.tag == "Player"){
+                    obj.GetComponent<PlayerHealth>().ExplosionDmg = DamageAmount;
                 }
+                else if(obj.TryGetComponent<DamageReceiver>(out DamageReceiver receiver)){
+                    receiver.HealthLevel -= (float)DamageAmount;
+                }
+            }
                 
             Destroy(gameObject);
         }
@@ -54,6 +46,4 @@ public class MinecartMovement : MonoBehaviour
     void DestroyMe(){
         Destroy(this.gameObject);
     }
-    
-
 }
