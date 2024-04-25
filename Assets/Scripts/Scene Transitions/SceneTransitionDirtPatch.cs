f3e2270e89fde162071f4b69bb7a6c4a7e45d2a8
Jodi Foster <33043348+JohhnSmithh@users.noreply.github.com>
2024-04-25 11:12:56 -0400

    nearby enemies must not be present to activate/use dirt patch

--- Assets/Scripts/Scene Transitions/SceneTransitionDirtPatch.cs
+++ Assets/Scripts/Scene Transitions/SceneTransitionDirtPatch.cs
@@ -8,6 +8,8 @@ public class SceneTransitionDirtPatch : MonoBehaviour
     [SerializeField] Collider _transitionCollider;
     [SerializeField] GameObject _glowParticles;
 
+    [SerializeField, Tooltip("Range within which there must be no enemies for patch to be usable")] private float _enemyCheckRadius = 5f;
+
     public bool CanMoveOn; // When we "clear" a scene, just toggle this
     void Start()
     {
@@ -18,10 +20,27 @@ public class SceneTransitionDirtPatch : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (CanMoveOn)
+        // check for nearby enemies
+        bool enemiesNearby = false;
+        Collider[] nearbyObjs = Physics.OverlapSphere(transform.position, _enemyCheckRadius);
+        foreach (var obj in nearbyObjs)
+        {
+            if (obj.CompareTag("Enemy"))
+            {
+                enemiesNearby = true;
+                break;
+            }
+        }
+
+        if (CanMoveOn && !enemiesNearby)
         {
             _transitionCollider.enabled = true;
             _glowParticles.SetActive(true);
+        }
+        else // allows deactivating if an enemy comes too close into range
+        {
+            _transitionCollider.enabled = false;
+            _glowParticles.SetActive(false);
         }
     }
 }
